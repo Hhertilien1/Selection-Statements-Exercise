0e610034f185b3b70ed92589761e1a8055eea5d8
Hhertilien1 <kinghertilien@gmail.com>
2023-12-13 15:29:55 -0500

    completed part 1 of excersize

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -4,7 +4,29 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            GuessingGame();
+        }
+
+        public static void GuessingGame()
+        {
+            Random r = new Random();
+            int FavNum = r.Next(1, 10);
+
+            Console.WriteLine("Guess my favorite number betwwn 1-10");
+            int response = int.Parse(Console.ReadLine());
+
+            if(response < FavNum)
+            {
+                Console.WriteLine("Too low!");
+            }
+            else if(response > FavNum)
+            {
+                Console.WriteLine("Too high!");
+            }
+            else
+            {
+                Console.WriteLine($"{response} was correct!");
+            }
         }
     }
 }
